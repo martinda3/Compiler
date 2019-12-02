00971684f07fc0a8aac539f12766c10fb605ab60
martinda3 <martinda3@vcu.edu>
2019-12-02 16:24:04 -0500

    Pushing and Popping local vairables

--- src/JVM/JVMgenerateExpression.cpp
+++ src/JVM/JVMgenerateExpression.cpp
@@ -16,7 +16,11 @@
 #include "TCglobals.h"
 #include "TCsymbol.h"
 #include "TCsymTable.h"
+#include "AStype.h"
 
+#include "line.h"
+#include "JVMgenUtils.h"
+#include "JVMgenGlobals.h"
 #include "ASexprState.h"
 
 #include "INEG.h"
@@ -29,9 +33,9 @@ namespace toyc {
             genExpression(be->getOp1(), tc);
             genExpression(be->getOp2(), tc);
             ASoperator *temp = be->getOper(); // This is easier do this!!!!!!!!!!!!
+            TCtoken *check1, *check2;
             TCtoken *op = temp->getExpr();    // This is easier do this!!!!!!!!!!!!
-            //std::string cleanup, idd;
-            //TCsymbol *tok;
+            std::string cleanup, idd; ASsimpleExpr *se;
             switch (op->getTokenType()) {
                 case ADDOP:
                     JVMgenUtils::gen_ADDOP(*op, tc);
@@ -49,13 +53,22 @@ namespace toyc {
                     JVMgenUtils::gen_AND(*op, tc);
                     break;
                 case ASSIGNOP:
-//                    cleanup = be->getOp2()->toString();
-//                    idd = cleanup.substr(4, (cleanup.length() -1) - 4);
-//                    loc = symTable->find(idd);
-//                    tok->setId(idd);
-//                    tok->setType(VAR);
-//                    tok->setValue(symTable->getSym(loc)->getValue());
-//                    JVMgenUtils::gen_ISTORE(*symTable->getSym(tok)->getId(),tc);
+                    se = dynamic_cast<ASsimpleExpr *>(be->getOp2());
+                    check1 = dynamic_cast<TCtoken *>(se->getExpr());
+//                    std::cout << check1->toString() << std::endl;
+
+                    se = dynamic_cast<ASsimpleExpr *>(be->getOp1());
+                    check2 = dynamic_cast<TCtoken *>(se->getExpr());
+//                    std::cout << check2->toString() << std::endl;
+
+                    if (check1->getTokenType() == ID && check2->getTokenType() == NUMBER){
+                        JVMgenUtils::gen_ICONST(stoi(check2->getLexeme()), tc);
+                        TCsymbol *idsym = symTable->getSym(symTable->find(check1->getLexeme()));
+                        JVMgenUtils::gen_ISTORE(*symTable->getSym(symTable->find(check1->getLexeme())),tc);
+                        tc->add(new line(LINE_COUNTER)); // Abirtaaaary value
+                        LINE_COUNTER++;
+                        JVMgenUtils::gen_ILOAD(*idsym, tc);
+                    }
                     break;
                 default: // shouldn't happen
 //                    getvale = dynamic_cast<ASexprState *>(be->getOp1());
@@ -72,10 +85,14 @@ namespace toyc {
             std::string lexeme = t->getLexeme();
             if (t->getTokenType() == ID) {
                 TCsymbol *idsym = symTable->getSym(symTable->find(lexeme));
-                JVMgenUtils::gen_ILOAD(*idsym, tc);
+                if (idsym->getValue() == "NULL") {
+                    JVMgenUtils::gen_ICONST(0, tc);
+                    JVMgenUtils::gen_ILOAD(*idsym, tc);
+                }
+//                //JVMgenUtils::gen_ILOAD(*idsym, tc);
             }
             else if (t->getTokenType() == NUMBER) {
-                JVMgenUtils::gen_ICONST(stoi(lexeme), tc);
+                //JVMgenUtils::gen_ICONST(stoi(lexeme), tc);
             }
             else { // shouldn't happen
                 std::cerr << "Fatal internal error #2: JVMgenerateExpression" << std::endl;
