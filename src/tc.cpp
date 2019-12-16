716348149261ecf70925b3b70a9fba271ff7b400
martinda3 <31861669+martinda3@users.noreply.github.com>
2019-12-15 22:02:02 -0500

    i = i + 1 works (#51)

--- src/tc.cpp
+++ src/tc.cpp
@@ -40,28 +40,39 @@ int main(int argc, char *argv[]) {
     strftime(buffer,sizeof(buffer),"%H:%M:%S",timeinfo);
     std::string ffile(buffer);
 
-    try
-    {
-        processCommandLine(argc,argv, ffile);
-        TClexer *scanner = new TClexer(inputFileName);
-        TCparser * parser = new TCparser(scanner);
-        ASabstractSyntax *ast = parser->parse();
+    TClexer *scanner;
+    TCparser * parser;
+    ASabstractSyntax *ast;
+    CGcodeGenerator *cg;
+    CGtargetCode* tc;
+    // Better for error checking (UNGLY)
+    try { processCommandLine(argc, argv, ffile); }
+    catch (...) { std::cerr << "ERROR: processCommandLine" << std::endl; exit(EXIT_FAILURE); }
 
-        CGcodeGenerator *cg = new JVMcodeGenerator();
-        CGtargetCode* tc = cg->generateCode(ast);
-        tc->writeCode(tc,targetFileName);
-        if (v_code_gen)
-        {
-//            dumpAST(ast);
-            dumpST(symTable);
-//            dumpCode(tc);
-        }
+    try { scanner = new TClexer(inputFileName); }
+    catch (...) { std::cerr << "ERROR: TClexer" << std::endl; exit(EXIT_FAILURE); }
+
+    try { parser = new TCparser(scanner); }
+    catch (...) { std::cerr << "ERROR: TCparser" << std::endl; exit(EXIT_FAILURE); }
+
+    try { ast = parser->parse(); }
+    catch (...) { std::cerr << "ERROR: ASabstractSyntax" << std::endl; exit(EXIT_FAILURE); }
+
+    try { cg = new JVMcodeGenerator(); }
+    catch (...) { std::cerr << "ERROR: CGcodeGenerator" << std::endl; exit(EXIT_FAILURE); }
+
+    try { tc = cg->generateCode(ast); }
+    catch (...) { std::cerr << "ERROR: CGtargetCode" << std::endl; exit(EXIT_FAILURE); }
+
+    try { tc->writeCode(tc,targetFileName); }
+    catch (...) { std::cerr << "ERROR: writeCode" << std::endl; exit(EXIT_FAILURE); }
+
+    if (v_code_gen)
+    {
+        dumpAST(ast);
+        dumpST(symTable);
+        dumpCode(tc);
     }
-    catch (...)
-        {
-        std::cerr << "ERROR: scanning failed" << std::endl;
-        exit(EXIT_FAILURE);
-        }
 }
 
 void processCommandLine(int argc, char *argv[], std::string filename) {
--- test/pt2.j
+++ test/pt2.j
@@ -1,7 +1,7 @@
 ;
 ; Source file: test/pt2.tc
 ; Target file: test/pt2.j
-; Sun Dec 15 16:30:36 2019
+; Sun Dec 15 21:59:30 2019
 ;
 
 .source test/pt2.tc
@@ -11,20 +11,27 @@
 .method public <init>()V
 	.limit stack 1
 	.limit locals 1
-.line 0
 	aload_0
 	invokespecial java/lang/Object/<init>()V
 	return
 .end method
 
 .method public static main([Ljava/lang/String;)V
-	.limit stack 2
-	.limit locals 3
-.line 1
+	.limit stack 10
+	.limit locals 10
+	iconst_1
 	bipush 10
+	iadd
 	istore_1
-.line 2
-.line 3
+	getstatic java/lang/System/out Ljava/io/PrintStream;
+	iload_1
+	invokevirtual java/io/PrintStream/println(I)V
+	iconst_1
+	iload_1
+	iadd
+	istore_1
+	getstatic java/lang/System/out Ljava/io/PrintStream;
+	iload_1
 	invokevirtual java/io/PrintStream/println(I)V
 	return
 .end method
